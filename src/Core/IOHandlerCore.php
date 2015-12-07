d23bcaf08596ef31cf94e393eefddd1c61e458db
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-12-08 00:21:33 +0900

    fix construct class

--- src/Core/IOHandlerCore.php
+++ src/Core/IOHandlerCore.php
@@ -16,12 +16,23 @@ class IOHandlerCore
     public static function execute($argv, $args_config)
     {
         $args = new Args($argv, $args_config);
+        $options = $args->getOptions();
         $exec_cls = 'App\\'.$args->getExecutableClass();
         $executable = new ReflectionMethod($exec_cls, $args->getExecutableMethod());
         $params = $executable->getParameters();
         $exec_args = [];
+        $input_counter = 0;
         foreach($params as $param){
-            var_dump($param->getClass());
+            $exec_cls = $param->getClass();
+            if(preg_match('/File\\*\\Input\Z/',$exec_cls->getName())){
+                $exec_args[] = new $exec_cls($options->i[$input_counter]);
+                $input_counter++;
+            }
+            if(preg_match('/File\\*\\Output\Z/', $exec_cls->getName())){
+                $tmp_name = mb_split('/\./', $options->i[0]);
+                $suffix = isset($options->s[0])?$options->s[0]:'';
+                $exec_args[] = new $exec_cls($tmp_name[0] . $suffix . date('Y-m-d_h_i_s') . "." . $tmp_name[1]);
+            }
         }
         if (empty($exec_args)) {
             $executable->invoke(null);
